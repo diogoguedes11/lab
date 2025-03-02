c11ab1ebf2d05b22c844e911a48db5726381b4aa
Diogo Guedes <diogo.guedes111@gmail.com>
2025-03-02 16:06:05 +0000

    google-network: add fw rule

--- google-cloud/networking/vpc.tf
+++ google-cloud/networking/vpc.tf
@@ -10,7 +10,17 @@ resource "google_compute_network" "vpc_network" {
 }
 resource "google_compute_subnetwork" "subnetwork" {
   name          = "${var.region}-subnet"
-  ip_cidr_range = "10.0.0.0/16"
+  ip_cidr_range = "10.0.0.0/24"
   region       = var.region 
   network= google_compute_network.vpc_network.name
+}
+
+resource "google_compute_firewall" "allow_ssh_fwrule" {
+  name    = "allow-ssh-fwrule"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = [ google_compute_subnetwork.subnetwork.ip_cidr_range  ]
 }
