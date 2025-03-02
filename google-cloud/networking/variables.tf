d48ccebbe0fb22833d704bd89dbdde3630db8125
Diogo Guedes <diogo.guedes111@gmail.com>
2025-03-02 15:39:28 +0000

    google-cloud: create vpc and subnet in my network project

---
+++ google-cloud/networking/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the project in which the resources will be created."
+  type        = string
+}
+
+variable "region" {
+     description = "The region in which the resources will be created."
+     type        = string
+}
---
+++ google-cloud/networking/vpc.tf
@@ -0,0 +1,16 @@
+provider "google" {
+     project = var.project_id
+     region  = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+
+}
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = "${var.region}-subnet"
+  ip_cidr_range = "10.0.0.0/16"
+  region       = var.region 
+  network= google_compute_network.vpc_network.name
+}
