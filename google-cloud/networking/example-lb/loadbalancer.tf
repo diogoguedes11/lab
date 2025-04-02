82d0a29ef89afcf249c86bcc00e367e2ba35842c
Diogo Guedes <diogo.guedes111@gmail.com>
2025-04-02 18:54:00 +0100

    feat: add networking infrastructure with VPC, subnetwork, compute instances, and load balancer

---
+++ google-cloud/networking/example-lb/loadbalancer.tf
@@ -0,0 +1,27 @@
+
+
+# forwarding rule
+resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
+  name                  = "ilb-forwarding-rule"
+  project               = local.project
+  backend_service       = google_compute_region_backend_service.default.id
+  provider              = google-beta
+  region                = "europe-west1"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "INTERNAL"
+  all_ports             = true
+  allow_global_access   = true
+  network               = google_compute_network.vpc_network.name
+  subnetwork            = google_compute_subnetwork.subnetwork.name
+
+}
+# health check
+resource "google_compute_health_check" "default" {
+  project  = local.project
+  name     = "ilb-hc"
+  provider = google
+  http_health_check {
+    port_specification = "USE_NAMED_PORT"
+    port_name          = "http"
+  }
+}
