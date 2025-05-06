a07b2d1af9592b02452b97e8af2775ffd6486ea9
Diogo Guedes <diogo.guedes111@gmail.com>
2025-05-06 22:01:14 +0100

    feat(google-autotag-idle-vms): read logs from google cloud logging

--- python/google-autotag-idle-vms/main.py
+++ python/google-autotag-idle-vms/main.py
@@ -1,15 +1,22 @@
 import google.auth
 import logging
+import google.cloud.logging
 if __name__ == "__main__":
     try:
         # Authentication
         credentials, project = google.auth.default()
-        
-        # Logging DEBUG
-        base_logger = logging.getLogger("google")
-        base_logger.addHandler(logging.StreamHandler())
-        base_logger.setLevel(logging.DEBUG)
-        
-    except:
-        print("An error occured.")
+        client = google.cloud.logging.Client()
+        logs = client.list_entries(page_size=10)
+        for entry in logs:
+            print(f"Timestamp: {entry.timestamp}")
+            print(f"Log Name: {entry.log_name}")
+            print(f"Resource: {entry.resource}")
+            print(f"Payload: {entry.payload}")
+            print(f"Severity: {entry.severity}")
+            print("-" * 40)
+    except google.auth.exceptions.DefaultCredentialsError as e:
+            print(f"Authentication error: {e}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
     
