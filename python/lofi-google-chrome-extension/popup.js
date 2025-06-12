4fd0c587f57d74543b138988a7222399368f8da0
Diogo Guedes <diogo.guedes111@gmail.com>
2025-06-12 15:41:02 +0100

    feat: update popup to include stream selection for Lofi music

--- python/lofi-google-chrome-extension/popup.js
+++ python/lofi-google-chrome-extension/popup.js
@@ -1,30 +1,10 @@
-const playlist = [{
-     title: "Lofi synthwave radio",
-     url: "https://www.youtube.com/watch?v=4xDzrJKXOOY",
-     thumbnail: "https://i.ytimg.com/vi/4xDzrJKXOOY/hqdefault.jpg"
-     }, {
-     title: "Lofi Girl",
-     url: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
-     thumbnail: "https://i.ytimg.com/vi/jfKfPfyJRdk/hqdefault.jpg"
-     }, {
-     title: "Lofi Hip Hop Radio",
-     url: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
-     thumbnail: "https://i.ytimg.com/vi/jfKfPfyJRdk/hqdefault.jpg"
-}]
+ const selector = document.getElementById("streamSelector");
+ const player = document.getElementById("lofiPlayer");
 
-
-function loadPlaylist() {
-     const playlistElement = document.getElementById('playlist');
-     playlist.forEach(item => {
-          const listItem=  document.createElement('li');
-          listItem.innerHTML = `
-               <a href="${item.url}" target="_blank">
-                    <img src="${item.thumbnail}" alt="${item.title}" style="width: 100px; height: 100px;">
-                    <p>${item.title}</p>
-               </a>
-          `;
-          playlistElement.appendChild(listItem);
-     })
-}
-
-loadPlaylist();
+    selector.addEventListener("change", () => {
+      const videoId = selector.value;
+      if (videoId) {
+        const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
+        player.src = embedUrl;
+      }
+    });
