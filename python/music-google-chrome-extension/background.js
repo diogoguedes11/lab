a9afe816eb757c2c256a8f483d81d80d933dee3a
Diogo Guedes <diogo.guedes111@gmail.com>
2025-06-15 21:27:46 +0100

    feat: music player extension changes

--- python/music-google-chrome-extension/background.js
+++ python/music-google-chrome-extension/background.js
@@ -9,14 +9,3 @@ chrome.action.onClicked.addListener((tab) => {
     height: windowHeight,
   });
 });
-
- const selector = document.getElementById("streamSelector");
- const player = document.getElementById("Player");
-
-    selector.addEventListener("change", () => {
-      const videoId = selector.value;
-      if (videoId) {
-        const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0`;
-        player.src = embedUrl;
-      }
-    });
