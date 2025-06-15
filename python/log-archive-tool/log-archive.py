4568363a74b606211981c1ba4c57666be34d37eb
Diogo Guedes <diogo.guedes111@gmail.com>
2025-06-15 22:50:07 +0100

    feat(log-archive): improvements

--- python/log-archive-tool/log-archive.py
+++ python/log-archive-tool/log-archive.py
@@ -1,7 +1,12 @@
 
 import argparse
+import os
 import subprocess
-import datetime
+from datetime import datetime
+
+DEFAULT_VAR_LOCATION = '/var/log'
+OUTPUT_DIR = './archive'
+
 def main():
      parser = argparse.ArgumentParser(
           prog="Log Archive tool",
@@ -9,9 +14,18 @@ def main():
      )
      parser.add_argument('-f','--logdir',help="Log directory path")
      args = parser.parse_args()
-     # if args.logdir:
-     #      print('hello')
-     datehour = f'{datetime.datetime.now().strftime("%Y%m%d")}_{datetime.datetime.now().strftime("%-I%-M%-S")}'
-     subprocess.run(['sudo','tar','-czf', f'log_archive_{datehour}.tar.gz','/var/log'])
+     datehour = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+     archive_name = f'log_archive_{datehour}.tar.gz'
+     archive_path = os.path.join(OUTPUT_DIR,archive_name)
+     print(archive_path)
+     logdir = args.logdir if args.logdir else DEFAULT_VAR_LOCATION
+     if not os.path.exists(logdir):
+          print(f"The path: {logdir} does not exist")
+     os.makedirs(OUTPUT_DIR, exist_ok=True)
+     try:
+          subprocess.run(['sudo','tar','-czf', archive_path, logdir])
+     except Exception as e:
+          print(f"Error while arquiving logs {str(e)}")
+     
 if __name__ == "__main__":
      main()
