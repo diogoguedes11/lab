b21230321dd933fb1e8cdc3f4d3190470ac11824
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-07-11 09:16:10 +0100

    feat: add Loki and Tempo Helm releases with configuration

--- google-cloud/public-gke-grafana-stack/grafana-stack.tf
+++ google-cloud/public-gke-grafana-stack/grafana-stack.tf
@@ -10,8 +10,29 @@ resource "helm_release" "grafana" {
   name       = "grafana"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
-  values     = file("${path.module}/manifests/grafana-values.yaml")
-  namespace  = "observability"
+  # values     = file("${path.module}/manifests/grafana-values.yaml")
+  namespace = "observability"
 }
 
 
+# ---------------------------------------------------------------------------------------------------------------------
+# Deploy Loki using Helm
+# ---------------------------------------------------------------------------------------------------------------------
+resource "helm_release" "loki" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki"
+  values     = file("${path.module}/manifests/loki-values.yaml")
+  namespace  = "observability"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Deploy Tempo using Helm
+# ---------------------------------------------------------------------------------------------------------------------
+resource "helm_release" "tempo" {
+  name       = "tempo"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "tempo"
+  # values     = file("${path.module}/manifests/tempo-values.yaml")
+  namespace = "observability"
+}
