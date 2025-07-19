5e0a4f091d79b89d2bfbce7ad9b7548d32ea7645
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-07-19 21:57:24 +0100

    feat: implement caching logic in proxy server with cache hit/miss handling

--- golang/caching-proxy/caching-proxy.go
+++ golang/caching-proxy/caching-proxy.go
@@ -3,17 +3,84 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"time"
 )
 
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
+type CacheEntry struct {
+	StatusCode int
+	Header http.Header
+	Body []byte
 }
 
+func makeHandler(origin string, cache map[string]CacheEntry ) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + ":" + origin + r.URL.RequestURI()
+
+	// check cache
+	cachedResponse , found := cache[key]
+	bodyBytes , err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalf("No body returned from the webpage %v", err)
+	}
+	if found {
+		for k, vv := range cachedResponse.Header {
+			for _, v := range vv {
+				w.Header().Add(k, v)
+			}
+			w.Header().Set("X-Cache", "HIT")
+			w.WriteHeader(cachedResponse.StatusCode)
+			w.Write(cachedResponse.Body)
+			return
+		}
+
+	} else {
+		// Cache miss
+		req, err := http.NewRequest(r.Method,origin+r.URL.RequestURI(), r.Body)
+		fmt.Println("Cache miss for key:", key)
+		if err != nil {
+			log.Fatalf("Failed to create request: %v", err)
+		}
+		req.Header = r.Header.Clone()
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		if err != nil {
+			http.Error(w, "Error fetching from origin server", http.StatusInternalServerError)
+			log.Printf("Error fetching from origin server: %v", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		if err != nil {
+			log.Printf("Error reading response body: %v", err)
+			return
+		}
+
+		cache[key] = CacheEntry{
+			StatusCode: resp.StatusCode,
+			Header:     resp.Header.Clone(),
+			Body:      bodyBytes,
+		}
+
+		for k , vv := range resp.Header {
+			for _, v := range vv {
+				w.Header().Add(k, v)
+			}
+		}
+		w.Header().Set("X-Cache", "MISS")
+		w.WriteHeader(resp.StatusCode)
+		_, err = w.Write(bodyBytes)}
+}
+
+}
+
+
 func main() {
+	cache := make(map[string]CacheEntry)
+
 	port := flag.String("port", "8080", "port to listen on")
 	origin := flag.String("origin", "http://localhost", "origin server URL")
 
@@ -28,7 +95,7 @@ func main() {
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
-		Handler: http.HandlerFunc(handleRequest),
+		Handler: makeHandler(*origin,cache),
 	}
 	log.Println("Starting proxy server on :8080")
 	err := s.ListenAndServe()
