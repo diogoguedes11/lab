c8d4c56dcec78ba52a0f68bbb2ef49a4ec06c9ca
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-07-22 10:58:16 +0100

    refactor: rename variable and improve text_to_morse function for clarity

--- python/morse-translator/main.py
+++ python/morse-translator/main.py
@@ -1,29 +1,39 @@
 # https://en.wikipedia.org/wiki/Morse_code#/media/File:International_Morse_Code.svg
 
-
-morse_code_dict = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 
-    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 
-    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 
-    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
-    'Y': '-.--', 'Z': '--..',
-    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', 
-    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
-    ',': '--..--', '.': '.-.-.-', '?': '..--..', "'": '.----.', '!': '-.-.--', 
-    '/': '/', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...', 
-    ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', 
-    '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+MORSE_CODE_DICT = {
+     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+     'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+     'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+     'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+     'Y': '-.--', 'Z': '--..',
+     '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+     '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+     ',': '--..--', '.': '.-.-.-', '?': '..--..', "'": '.----.', '!': '-.-.--',
+     '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...', ':': '---...',
+     ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-',
+     '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
 }
 
+def text_to_morse(msg: str) -> str:
+     """
+     Convert a text message to Morse code.
+     Spaces are converted to '/'.
+     Unknown characters are ignored.
+     """
+     morse_msg = []
+     for char in msg:
+          if char == ' ':
+               morse_msg.append('/')
+          else:
+               code = MORSE_CODE_DICT.get(char.upper())
+               if code:
+                    morse_msg.append(code)
+               # else: ignore unknown characters
+     return ' '.join(morse_msg)
 
-def text_to_morse(msg):
-
-  morse_msg = [morse_code_dict[char.upper()] for char in msg]
-
-  morse_msg_str = ' '.join(morse_msg)
-
-  return morse_msg_str
+def main():
+     msg = input("Send me a message you want to translate to Morse: ")
+     print(text_to_morse(msg))
 
 if __name__ == '__main__':
-  msg = input("Send me an message you want to translate to morse: ").replace(" ","/")
-  print(text_to_morse(msg))
+     main()
