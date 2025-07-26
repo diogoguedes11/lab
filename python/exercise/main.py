137224600e3d20050ee6313c55e07f065dd17172
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-07-26 17:16:17 +0100

    refactor: simplify process CPU usage aggregation logic

--- python/exercise/main.py
+++ python/exercise/main.py
@@ -8,10 +8,12 @@ def main():
           read = f.readlines()
           for row in read[1:]:
                line = row.split(",")
-               if line[0] not in process_usage:
-                    process_usage[line[0]] = int(line[2].strip("\n"))
+               process = line[0]
+               cpu_usage = line[2]
+               if process not in process_usage:
+                    process_usage[process] = int(cpu_usage.strip("\n"))
                else:
-                    process_usage[line[0]] += int(line[2].strip("\n"))
+                    process_usage[process] += int(cpu_usage.strip("\n"))
      sorted_processes = sorted(process_usage.items(), key=lambda item: int(item[1]), reverse=True)
      for process,cpu in sorted_processes[:3]:
           print("========================================")
