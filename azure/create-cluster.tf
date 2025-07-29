392982cafc4381ada821f88785e59c000ffef618
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-07-29 17:37:45 +0100

    feat: add Azure Key Vault configuration and nginx deployment with secrets integration

--- azure/create-cluster.tf
+++ azure/create-cluster.tf
@@ -20,8 +20,33 @@ resource "azurerm_kubernetes_cluster" "this" {
 
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.this.kube_config_raw
+resource "azurerm_key_vault" "this" {
+  name                     = "tst-keyvaultdeleteme"
+  location                 = var.location
+  resource_group_name      = var.resource_group_name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = false
 
-  sensitive = true
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get", "List", "Set", "Purge"
+    ]
+  }
+  access_policy { # Managed identity
+    tenant_id               = data.azurerm_client_config.current.tenant_id
+    object_id               = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+    secret_permissions      = ["Get", "List"]
+    key_permissions         = ["Get", "List"]
+    certificate_permissions = ["Get", "List"]
+  }
+
+
 }
+
+
+
+data "azurerm_client_config" "current" {}
