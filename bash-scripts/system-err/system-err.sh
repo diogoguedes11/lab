03bd02c83b06c926eda25087d59cd13f8be92cab
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-08-01 12:35:40 +0100

    feat: refactor system error message script to improve structure and readability

--- bash-scripts/system-err/system-err.sh
+++ bash-scripts/system-err/system-err.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
 
-top_err=$(journalctl -p err -b)
+# Show system boot errors
+boot_errors() {
+     echo "You chose: $1"
+     echo "Errors from system boot:"
+     journalctl -p err -b
+}
+# Filter errors by service name
+filter_services() {
+     local service=$1
+     if [[ -z "$service" ]]; then
+          echo "Service name cannot be empty. Please try again."
+          continue
+     fi
+     filter_err=$(journalctl -p err -b --grep "$service")
+     if [[ -z "$filter_err" ]]; then
+          echo "No errors found for service: $service"
+     else
+          echo "$filter_err"
+     fi
+
+}
 
 
-while true; do
-read -p "Choose between [boot_errors],[filter_err] to get the system error messages: " option
-clear
-case "$option" in 
-     boot_errors)
-               echo "You chose: $option"
-               echo "Errors from system boot:"
-               echo "$top_err"
+main() {
+     while true; do
+          read -p "Choose between [boot_errors],[filter_err] to get the system error messages or [exit] to leave the program: " option
+          clear
+          case "$option" in 
+          boot_errors)
+                    boot_errors "$option"
+                    ;;
+
+          filter_err)
+                    read -p "Which service do you want to filter on? ex: [dmesg]:  " service
+                    filter_services "$service"
+                    ;;
+
+          exit)
+               exit 0
                ;;
+          *)
+               echo  "Invalid option. Please choose either boot_errors or filter_err."
+               ;;
+          esac
+     done
+}
 
-     filter_err)
-          read -p "Which service do you want to filter on? ex: [dmesg]:  " service
-            if [[ -z "$service" ]]; then
-                echo "Service name cannot be empty. Please try again."
-                continue
-            fi
-            filter_err=$(journalctl -p err -b --grep "$service")
-            if [[ -z "$filter_err" ]]; then
-                echo "No errors found for service: $service"
-            else
-                echo "$filter_err"
-            fi
-            ;;
-
-     *)
-          echo  "Invalid option. Please choose either boot_errors or filter_err."
-          ;;
-     esac
-done
+main
