1c37b39481b64d7443c0ebd291f7f8e1b9090cae
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2025-08-08 21:30:17 +0100

    feat: refactor pomodoro timer structure and enhance command-line interface

--- python/pomo-cli/pomo-cli.py
+++ python/pomo-cli/pomo-cli.py
@@ -1,7 +1,7 @@
 import datetime
 import vlc
-import os
 import time
+import argparse
 def get_current_time():
      return datetime.datetime.now() 
 
@@ -15,15 +15,21 @@ def pause_notification() -> None:
      time.sleep(3)  # Allow time for the sound to start playing
      print("You did it! Make yourself proud for one more pomodoro. Keep Grinding. But never forget to take a break.")
 
-
-if __name__ == '__main__':
+def start_pomodoro() -> None:
      current_time = get_current_time()
      next_pause = thirty_minutes_timer(current_time)
      print(f"Pomodoro started. Focus")
      print(f"Next break at: {next_pause.strftime('%H:%M:%S')}")
-    
      while True:
           if datetime.datetime.now() >= next_pause:
                pause_notification()
                break
           time.sleep(1)
+
+
+if __name__ == '__main__':
+     parser = argparse.ArgumentParser(prog="pomo",description="Pomodoro timer")
+     parser.add_argument("command", choices=["start","stop"], help="Command to be executed")
+     args = parser.parse_args()
+     if args.command == 'start':
+          start_pomodoro()
