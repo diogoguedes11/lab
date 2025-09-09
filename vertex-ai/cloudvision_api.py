7fa7bc4269d3e02a32f9fdd57a445ebd90a31e6a
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-09-09 09:23:05 +0100

    Add Jupyter Notebook for Vertex AI Image Label Detection
    - Created a new notebook for demonstrating image label detection using Vertex AI.
    - Included necessary imports and initialized Vertex AI.
    - Implemented label detection on a sample image of a cat, displaying the results and labels detected.
    - Added markdown documentation for clarity and context.

--- vertex-ai/cloudvision_api.py
+++ vertex-ai/cloudvision_api.py
@@ -1,9 +1,18 @@
 from google.cloud import vision
 
 
-def detect_labels(image_path):
+def detect_labels(image_path: str):
      client = vision.ImageAnnotatorClient()
-     file_uri = "./image.png"
+     file_uri = image_path
      
      image = vision.Image()
      image.source.image_uri = file_uri
+     # Performs label detection on the image file
+     response = client.label_detection(image=image)
+     labels = response.label_annotations
+     # print(response,image)
+     print("Labels:")
+     for label in labels:
+          print(label.description)
+
+     return labels
