6ea4293f92525af8aafc3be1deb868346b01f62c
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-09-09 09:47:13 +0100

    feat: update label detection logic to improve handling of local files and clean notebook outputs

--- vertex-ai/cloudvision_api.py
+++ vertex-ai/cloudvision_api.py
@@ -1,17 +1,23 @@
 from google.cloud import vision
-
+import os
 
 def detect_labels(image_path: str):
      client = vision.ImageAnnotatorClient()
-     file_uri = image_path
-     external_url_prefix = ['gs://','https://','http://']
-     if external_url_prefix in image_path:
+
+     if 'gs://' in image_path or 'https://' in image_path or 'http://' in image_path:
           image = vision.Image()
+          file_uri = image_path
           image.source.image_uri = file_uri
           # Performs label detection on the image file
           response = client.label_detection(image=image)
           labels = response.label_annotations
-          # print(response,image)
+     else:
+          file_name = os.path.abspath(image_path)
+          with open(file_name, 'rb') as image_file:
+               content = image_file.read()
+          image = vision.Image(content=content)
+          response = client.label_detection(image=image)
+          labels = response.label_annotations
      print("Labels:")
      for label in labels:
           print(label.description)
