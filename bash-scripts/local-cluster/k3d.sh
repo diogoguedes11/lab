b60403eb32a9577acc596b6c9b0ec77da47a46eb
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-09-29 17:42:21 +0100

    feat: add shell script local cluster

---
+++ bash-scripts/local-cluster/k3d.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Prerequisites:
+# - Docker
+# - k3d
+# - kubectl
+
+# install k3d with:
+# wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+sudo k3d cluster create lab
+sudo k3d kubeconfig merge lab --kubeconfig-switch-context
+sudo mv /root/.config/k3d/kubeconfig-lab.yaml ~/.kube/config
+sudo chmod 607 ~/.kube/config
+kubectl get pods
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
