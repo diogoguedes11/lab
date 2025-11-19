63778f39f4c13f409081ccdc65e062d8c1c5cabc
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2025-11-19 16:56:52 +0000

    feat: appgw-infra script update

--- azure/powershell/appgw-infra.ps1
+++ azure/powershell/appgw-infra.ps1
@@ -1,5 +1,6 @@
 #Pref
 $WarningPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
 
 #Variables
 $rg = "rg-appgw"
@@ -12,7 +13,7 @@ $password = ConvertTo-SecureString $plainPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($username, $password);
 
 # Create Resource Group
-New-AzResourceGroup -Name $rg -Location $region
+New-AzResourceGroup -Name $rg -Location $region -Force
 
 # Create VNet and Subnets
 $vnetName = "vnet-appgw"
@@ -22,7 +23,10 @@ $contacts = New-AzVirtualNetworkSubnetConfig -Name "snet-appgw-contacts" -Addres
 $help = New-AzVirtualNetworkSubnetConfig -Name "snet-appgw-help" -AddressPrefix "10.0.1.0/24"
 
 # Creating the VNet with the subnets
-New-AzVirtualNetwork -ResourceGroupName $rg -Location $region -Name $vnetName -AddressPrefix "10.0.0.0/16" -Subnet $contacts, $help
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $rg -Name $vnetName -ErrorAction SilentlyContinue
+if (-not $vnet) {
+    New-AzVirtualNetwork -ResourceGroupName $rg -Location $region -Name $vnetName -AddressPrefix "10.0.0.0/16" -Subnet $contacts, $help
+}
 
 # Create virtual machines in each subnet
 
@@ -31,16 +35,18 @@ $contactsScript = @"
 #!/bin/bash
 apt-get update
 apt-get install -y nginx
-sudo -s
-echo 'This is the page contacts' > /var/www/html/index.html
-systemctl restart nginx
+echo 'This is the page contacts' | sudo tee /var/www/html/index.html
+sudo systemctl restart nginx
 "@
 
-$contactsNic = New-AzNetworkInterface -ResourceGroupName $rg -Location $region -Name "nic-contacts" -SubnetId (Get-AzVirtualNetwork -ResourceGroupName $rg -Name $vnetName).Subnets[0].Id
+# Create public IP for contacts VM
+$contactsPublicIp = New-AzPublicIpAddress -ResourceGroupName $rg -Location $region -Name "pip-contacts" -AllocationMethod Static -Sku Standard
+
+$contactsNic = New-AzNetworkInterface -ResourceGroupName $rg -Location $region -Name "nic-contacts" -SubnetId (Get-AzVirtualNetwork -ResourceGroupName $rg -Name $vnetName).Subnets[0].Id -PublicIpAddressId $contactsPublicIp.Id
 
 $contactsVmConfig = New-AzVmConfig -VMName "vm-contacts" -VMSize "Standard_B1s" | `
-   Set-AzVmOperatingSystem -Linux -ComputerName "vm-contacts" -Credential $credential | `
-   Set-AzVmSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "18.04-LTS" -Version "latest" | `
+   Set-AzVmOperatingSystem -Linux -ComputerName "vm-contacts" -Credential $credential -DisablePasswordAuthentication:$false | `
+   Set-AzVmSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
    Add-AzVmNetworkInterface -Id $contactsNic.Id
 
 New-AzVM -ResourceGroupName $rg -Location $region -VM $contactsVmConfig
@@ -53,19 +59,26 @@ $helpScript = @"
 #!/bin/bash
 apt-get update
 apt-get install -y nginx
-sudo -s
-echo 'This is the page help' > /var/www/html/index.html
-systemctl restart nginx
+echo 'This is the page help' | sudo tee /var/www/html/index.html
+sudo systemctl restart nginx
 "@
 
-$helpNic = New-AzNetworkInterface -ResourceGroupName $rg -Location $region -Name "nic-help" -SubnetId (Get-AzVirtualNetwork -ResourceGroupName $rg -Name $vnetName).Subnets[1].Id
+# Create public IP for help VM
+$helpPublicIp = New-AzPublicIpAddress -ResourceGroupName $rg -Location $region -Name "pip-help" -AllocationMethod Static -Sku Standard
+
+$helpNic = New-AzNetworkInterface -ResourceGroupName $rg -Location $region -Name "nic-help" -SubnetId (Get-AzVirtualNetwork -ResourceGroupName $rg -Name $vnetName).Subnets[1].Id -PublicIpAddressId $helpPublicIp.Id
+
 
 $helpVmConfig = New-AzVmConfig -VMName "vm-help" -VMSize "Standard_B1s" | `
-   Set-AzVmOperatingSystem -Linux -ComputerName "vm-help" -Credential $credential | `
-   Set-AzVmSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "18.04-LTS" -Version "latest" | `
+   Set-AzVmOperatingSystem -Linux -ComputerName "vm-help" -Credential $credential -DisablePasswordAuthentication:$false | `
+   Set-AzVmSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
    Add-AzVmNetworkInterface -Id $helpNic.Id
 
 New-AzVM -ResourceGroupName $rg -Location $region -VM $helpVmConfig
 
 # Run custom script on help VM
 Invoke-AzVMRunCommand -ResourceGroupName $rg -VMName "vm-help" -CommandId 'RunShellScript' -ScriptString $helpScript
+
+# Output public IP addresses
+Write-Host "Contacts VM Public IP: $($contactsPublicIp.IpAddress)"
+Write-Host "Help VM Public IP: $($helpPublicIp.IpAddress)"
