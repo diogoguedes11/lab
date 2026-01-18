85e79547d571991256afc4e524b99e0977e57755
Diogo Guedes <diogo.guedes111@gmail.com>
2026-01-18 16:21:01 +0000

    feat: add GCP infrastructure configuration files

---
+++ 00-infrastructure-as-code/gcp/ncc/main.tf
@@ -0,0 +1,51 @@
+resource "google_network_connectivity_hub" "hub" {
+  name        = "star"
+  description = "A sample star hub"
+  labels = {
+    label-one = "value-one"
+  }
+  preset_topology = "STAR"
+}
+
+# Spoke1
+resource "google_compute_network" "vpc_spoke1" {
+  name                    = "vpc-spoke1"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "subnet_spoke1" {
+  name          = "sb-spoke1"
+  network       = google_compute_network.vpc_spoke1.name
+  ip_cidr_range = "10.0.0.0/24"
+  depends_on    = [google_compute_network.vpc_spoke1]
+}
+
+resource "google_network_connectivity_spoke" "spoke1" {
+  name     = "spoke1"
+  hub      = google_network_connectivity_hub.hub.name
+  location = "global"
+  linked_vpc_network {
+    uri = google_compute_network.vpc_spoke1.self_link
+  }
+
+}
+
+# Spoke 2
+resource "google_compute_network" "vpc_spoke2" {
+  name                    = "vpc-spoke2"
+  auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "subnet_spoke2" {
+  name          = "sb-spoke2"
+  network       = google_compute_network.vpc_spoke2.name
+  ip_cidr_range = "10.0.1.0/24"
+  depends_on    = [google_compute_network.vpc_spoke2]
+}
+
+resource "google_network_connectivity_spoke" "spoke2" {
+  name     = "spoke2"
+  hub      = google_network_connectivity_hub.hub.name
+  location = "global"
+  linked_vpc_network {
+    uri = google_compute_network.vpc_spoke2.self_link
+  }
+}
---
+++ 00-infrastructure-as-code/gcp/ncc/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "Google Project id"
+  type        = string
+}
+variable "region" {
+  description = "Region GCP"
+  type        = string
+}
