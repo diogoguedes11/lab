c9f27faf33d010a5d5f286d06021c2c623b90e3a
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-01-26 09:57:38 +0000

    feat: update firewall configurations for Node02 and adjust region settings

--- 00-infrastructure-as-code/gcp/linux-vms/main.tf
+++ 00-infrastructure-as-code/gcp/linux-vms/main.tf
@@ -68,48 +68,9 @@ resource "google_compute_subnetwork" "subnet_node02" {
 #   depends_on = [google_compute_subnetwork.subnet_node01]
 # }
 
-resource "google_compute_firewall" "allow_ssh_iap_node01" {
-  name    = "allow-ssh-via-iap"
-  network = google_compute_network.vpc_node01.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["35.235.240.0/20"]
-}
-
-resource "google_compute_firewall" "allow_icmp_node01" {
-  name    = "allow-icmp-node01"
-  network = google_compute_network.vpc_node01.self_link
-
-  allow {
-    protocol = "icmp"
-  }
-
-  source_ranges = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-}
-
-resource "google_compute_firewall" "allow_internal_node01" {
-  name    = "allow-internal-node01"
-  network = google_compute_network.vpc_node01.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["0-65535"]
-  }
-
-  allow {
-    protocol = "udp"
-    ports    = ["0-65535"]
-  }
-  source_ranges = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-}
-
-# resource "google_compute_firewall" "allow_ssh_iap_node02" {
-#   name    = "allow-ssh-via-iap-node02"
-#   network = google_compute_network.vpc_node02.self_link
+# resource "google_compute_firewall" "allow_ssh_iap_node01" {
+#   name    = "allow-ssh-via-iap"
+#   network = google_compute_network.vpc_node01.self_link
 
 #   allow {
 #     protocol = "tcp"
@@ -119,9 +80,9 @@ resource "google_compute_firewall" "allow_internal_node01" {
 #   source_ranges = ["35.235.240.0/20"]
 # }
 
-# resource "google_compute_firewall" "allow_icmp_node02" {
-#   name    = "allow-icmp-node02"
-#   network = google_compute_network.vpc_node02.self_link
+# resource "google_compute_firewall" "allow_icmp_node01" {
+#   name    = "allow-icmp-node01"
+#   network = google_compute_network.vpc_node01.self_link
 
 #   allow {
 #     protocol = "icmp"
@@ -130,9 +91,9 @@ resource "google_compute_firewall" "allow_internal_node01" {
 #   source_ranges = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 # }
 
-# resource "google_compute_firewall" "allow_internal_node02" {
-#   name    = "allow-internal-node02"
-#   network = google_compute_network.vpc_node02.self_link
+# resource "google_compute_firewall" "allow_internal_node01" {
+#   name    = "allow-internal-node01"
+#   network = google_compute_network.vpc_node01.self_link
 
 #   allow {
 #     protocol = "tcp"
@@ -143,9 +104,48 @@ resource "google_compute_firewall" "allow_internal_node01" {
 #     protocol = "udp"
 #     ports    = ["0-65535"]
 #   }
-
 #   source_ranges = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 # }
+
+resource "google_compute_firewall" "allow_ssh_iap_node02" {
+  name    = "allow-ssh-via-iap-node02"
+  network = google_compute_network.vpc_node02.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
+
+resource "google_compute_firewall" "allow_icmp_node02" {
+  name    = "allow-icmp-node02"
+  network = google_compute_network.vpc_node02.self_link
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_internal_node02" {
+  name    = "allow-internal-node02"
+  network = google_compute_network.vpc_node02.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
 
 resource "google_compute_instance" "linux_vm_node02" {
   name         = "linux-vm-node02"
@@ -239,7 +239,7 @@ resource "google_compute_instance_template" "instance_template" {
 
 resource "google_compute_region_health_check" "default" {
   name   = "l4-ilb-hc"
-  region = "europe-west1"
+  region = var.region
   http_health_check {
     port = "80"
   }
@@ -247,7 +247,7 @@ resource "google_compute_region_health_check" "default" {
 
 resource "google_compute_region_instance_group_manager" "mig" {
   name   = "l4-ilb-mig1"
-  region = "europe-west1"
+  region = var.region
   version {
     instance_template = google_compute_instance_template.instance_template.id
     name              = "primary"
