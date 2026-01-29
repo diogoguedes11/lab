04aa8cd0cef51faa61c642b50aaa028cb8283dc5
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-01-29 09:49:39 +0000

    Refactor code structure for improved readability and maintainability

--- 02-cloud-native-tooling/python/summarize-yt-ai/main.py
+++ 02-cloud-native-tooling/python/summarize-yt-ai/main.py
@@ -1,22 +1,30 @@
-import yt_dlp
 from yt_dlp import YoutubeDL
+import whisper 
 
-URL= 'https://www.youtube.com/watch?v=jkFal52bznw'
+
+URL= 'https://www.youtube.com/watch?v=21mDekTZwsw'
 def main():
     print("Starting YouTube video summarization...")
     ydl_opts = {
         'format': 'bestaudio/best',
+        'quiet': False,
+        'no_warnings': False,
+        'source_address': '0.0.0.0', 
+        'extractor_args': {'youtube': {'player_client': ['android']}},
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': 'audio_transcrever.%(ext)s'
-    }
+        'outtmpl': 'audio_transcrever',
+}
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download(URL)
-    print("Download concluído: audio_transcrever.mp3")
+    print("Download finished: audio_transcrever.mp3")
+    model = whisper.load_model("base")
+    result = model.transcribe("./audio_transcrever.mp3",fp16=False)
+    print(result["text"])
 
 if __name__ == "__main__":
     main()
