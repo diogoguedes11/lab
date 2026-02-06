af8816b3940fe11e94b598b31bb86c6c1e74b519
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-06 17:22:17 +0000

    feat: enhance script to create cgroups with proper isolation and permissions

--- 05-networking/scripts/cgroups/script.sh
+++ 05-networking/scripts/cgroups/script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Create the isolation with unshare
-unshare --fork --cgroup --net --mount-proc --uts --pid
+sudo unshare --fork --pid --mount-proc --uts --net --ipc --user --map-root-user /bin/bash
 
 # Testing 
 unshare /bin/bash
 ps -A
+# create cgroups
+mkdir /sys/fs/cgroup/container
+cd /sys/fs/cgroup/container
+echo "+memory" > cgroup.subtree_control
+echo "30M" > memory.max
