353ecad8bff00538b5cf98bab1cecf891892ca79
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-06 17:29:48 +0000

    feat: add memory limit checks and stress test to cgroup script

--- 05-networking/scripts/cgroups/script.sh
+++ 05-networking/scripts/cgroups/script.sh
@@ -11,3 +11,13 @@ mkdir /sys/fs/cgroup/container
 cd /sys/fs/cgroup/container
 echo "+memory" > cgroup.subtree_control
 echo "30M" > memory.max
+# Run a process in the cgroup
+echo $$ > cgroup.procs
+# Check the memory limit
+cat memory.max
+# Run a process that exceeds the memory limit
+stress --vm 1 --vm-bytes 50M --timeout 10s
+# Check the memory usage
+cat memory.current
+
+
