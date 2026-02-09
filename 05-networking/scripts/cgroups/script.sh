dceb1118ee61ec62893946db837c1ac3e01c0571
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-09 10:29:33 +0000

    fix: add comment to clarify process addition in cgroup script

--- 05-networking/scripts/cgroups/script.sh
+++ 05-networking/scripts/cgroups/script.sh
@@ -4,7 +4,7 @@ unshare --fork --pid --mount-proc bash
 
 mkdir /sys/fs/cgroup/cpu/cg1
 
-echo $$ > /sys/fs/cgroup/cpu/cg1/cgroup.procs
+echo $$ > /sys/fs/cgroup/cpu/cg1/cgroup.procs  # Automatically puts new processes in the cgroup (fork() will create a new process)
 
 echo "+cpu +memory -io" > /sys/fs/cgroup/<parent>/cgroup.subtree_control
 
