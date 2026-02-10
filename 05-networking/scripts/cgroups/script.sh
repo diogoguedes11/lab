b3461be6e3821bd5607eba5c3894f532f683f083
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-10 15:00:51 +0000

    fix: correct syntax for root user check and enhance cgroup configuration output

--- 05-networking/scripts/cgroups/script.sh
+++ 05-networking/scripts/cgroups/script.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 
+if ["$EUID" -ne 0]; then
+    echo "run as root"
+    exit
+fi
+
 create_namespace(){
     echo "Creating linux namespace..." 
     unshare --fork --pid --mount-proc bash
 
 }
 configure_cgroup(){
-
     local cgroup_name="$1"
     local memory_max="100M"
     local cpu_max="50000 100000"
+
+    echo "Configuring Cgroups v2..."
     mkdir /sys/fs/cgroup/cpu/${cgroup_name}
     echo "+cpu +memory -io" > /sys/fs/cgroup/${cgroup_name}/cgroup.subtree_control
     echo $$ > /sys/fs/cgroup/cpu/${cgroup_name}/cgroup.procs  # Automatically puts new processes in the cgroup (fork() will create a new process)
