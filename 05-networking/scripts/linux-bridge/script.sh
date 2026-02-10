9a6b6d3905d97dd30078a8c701e500c0ecfbfcb4
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-10 15:53:35 +0000

    feat: parameterize bridge and end host creation in networking script

--- 05-networking/scripts/linux-bridge/script.sh
+++ 05-networking/scripts/linux-bridge/script.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "You're not running as root user."
+    exit
+fi
 
 
 create_bridge(){
-  local nsname="bridge1"
-  local ifname="br1"
+  local nsname="$1"
+  local ifname="$2"
 
   echo "creating bridge ${nsname}/${ifname}"
   ip netns add ${nsname}
@@ -16,18 +20,28 @@ create_bridge(){
 
 
 create_end_host(){
-        local host_nsname="host-ns"
-        local bridge_nsname="bridge1"
-        local bridge_ifname="br1"
-        local peer1_ifname="eth-peer1"
-        local peer2_ifname="eth-peer2"
+        local host_nsname="$1"
+        local host_ifname="$2"
+        local bridge_nsname="$3"
+        local bridge_ifname="$4"
+        local bridge_peer_ifname="$5"
+        local host_ip="$6"
 
-        echo "creating end host ${host_nsname} connected to ${bridge_nsname}/${bridge_ifname}"
+        echo "---Connecting ${host_nsname} to Bridge ${bridge_ifname}"
         # Create end host namespace
         ip netns add ${host_nsname}
         ip netns exec ${host_nsname} ip link set lo up
 
+        # Create a veth pair connecting end host and bridge namespace
+        ip link add ${host_ifname} netns ${host_nsname} type veth pair name ${bridge_ifname} netns ${bridge_nsname}
+        ip link exec ${host_nsname} ip link set ${host_ifname} up
+        ip link exec ${bridge_nsname} ip link set ${bridge_peer_ifname} up
 
+        # Add address space
+        ip link exec ${host_nsname} exec ip adr add ${host_ip} dev ${host_ifname}
+
+        # Attach  host interface to the bridge
+        ip netns exec ${host_nsname} ip link set ${bridge_peer_ifname} master ${bridge_ifname}
 }
 
 
